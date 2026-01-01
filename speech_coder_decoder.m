8f30fccf37a1ad17874368e8f213197c672dcc04
Samuel Tesfachew <samuel.tesfachew@astu.edu.et>
2026-01-01 08:20:35 +0300

    First Commit

---
+++ speech_coder_decoder.m
@@ -0,0 +1,45 @@
+%%
+[speechSignal, Fs] = audioread('ahem_x.wav');
+%%
+downSampleFactor = 2;
+compressedSignal = downsample(speechSignal, downSampleFactor);
+compressedFs = Fs / downSampleFactor;
+%%
+reconstructedSignal = upsample(compressedSignal, downSampleFactor);
+
+lpFilter = designfilt('lowpassfir', ...
+    'PassbandFrequency', 0.4, ...
+    'StopbandFrequency', 0.5, ...
+    'DesignMethod', 'kaiserwin');
+
+reconstructedSignal = filter(lpFilter, reconstructedSignal);
+%%
+sound(speechSignal, Fs);
+pause(length(speechSignal)/Fs + 1);
+
+sound(reconstructedSignal, Fs);
+%%
+t1 = (0:length(speechSignal)-1)/Fs;
+t2 = (0:length(reconstructedSignal)-1)/Fs;
+
+figure;
+subplot(2,1,1);
+plot(t1, speechSignal);
+title('Original Speech Signal');
+
+subplot(2,1,2);
+plot(t2, reconstructedSignal);
+title('Reconstructed Speech Signal');
+%%
+audiowrite('reconstructed_speech.wav', reconstructedSignal, Fs);
+%%
+figure;
+plot(t1, speechSignal, 'b');
+hold on;
+plot(t2, reconstructedSignal, 'r');
+hold off;
+
+legend('Original', 'Reconstructed');
+title('Comparison of Speech Signals');
+xlabel('Time (s)');
+ylabel('Amplitude');
